b0a47f7ea9c0c2ee4375c38b3a42331de581d253
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2026-07-08 20:22:05 +0530

    feat: Update header and introduce new font

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,20 +2,19 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="w-full border-b border-gray-200">
-        <div className="flex w-full px-15 py-4 ">
-            <div className="flex gap-8 items-baseline">
-                <div className="flex items-center gap-2">
+    <header className="w-full border-b border-gray-300">
+        <div className="flex max-w-7xl w-full mx-auto px-15 py-4 ">
+            <div className="flex gap-12">
+                <div className="flex items-center">
                     <Image
-                        src="/logo.png"
+                        src="/logo1.png"
                         alt="Shotcode Logo"
-                        width={100}
-                        height={100}
+                        width={280}
+                        height={280}
                     />
                 </div>
-                <div className="text-xl font-medium text-black">ShotCode</div>
-                <div>
-                    <span className="text-xs text-gray-400">Build Better Components ;D</span>
+                <div className="mt-6">
+                    <span className="text-sm text-gray-400">Build Better Components :D</span>
                 </div>
             </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,26 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <main className="relative min-h-screen overflow-hidden">
+      {/* <span className="font-md" style={{fontFamily: "Gelica"}}>
+        Design interfaces.<br/> Let AI write the components
+      </span> */}
+      <div className="mx-auto flex max-w-7xl flex-col gap-8 px-15 pt-20 md:flex-row md:items-start md:justify-between">
+        <div className="max-w-xl">
+          <h1 className="font-serif font-medium text-5xl leading-tight tracking-tight text-black md:text-6xl">
+            Design interfaces. Let <br/>AI write the{" "} 
+            <span className="bg-green-400 px-2 py-[0px] leading-none">code</span>
+          </h1>
+
+          <button className="mt-8 rounded-full bg-blue px-6 py-3 text-sm font-medium text-mdium text-white hover: bg-gray-800 transition-colors">
+            Generate with AI
+          </button>
+        </div>
+        <p className="max-w-xs text-sm leading-relaxed text-gray-500 md:pt-10">
+          Instantly transform your component designs into clean,
+          production-safe React or Vue code without manual wiring
+        </p>
+      </div>
+    </main>
   );
 }
